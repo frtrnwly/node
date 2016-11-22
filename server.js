160403666d1781dd589fa7609a35376f601244ad
willy <willy@HP>
2016-11-21 19:33:01 -0500

    testing

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-﻿var http = require('http');
+﻿// my first node project
+
+var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
