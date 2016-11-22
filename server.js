55ce400942ad3a0271556392d028c40e72544c9f
willy <willy@HP>
2016-11-21 19:41:52 -0500

    new commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿// my first node project - test
 
 var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 80;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
